6e4608afa36a82bf4acf155928cbbb222e6f1999
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-08-27 13:14:29 +0100

    Custom trees generate from saplings without bonemeal.

--- src/main/java/appalachia/block/BlockSugiSapling.java
+++ src/main/java/appalachia/block/BlockSugiSapling.java
@@ -1,5 +1,6 @@
 package appalachia.block;
 
+import java.util.List;
 import java.util.Random;
 
 import net.minecraft.block.BlockBush;
@@ -9,110 +10,135 @@ import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
 
 import net.minecraftforge.event.terraingen.TerrainGen;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import appalachia.gui.AppalachiaTabs;
 import appalachia.world.gen.feature.tree.WorldGenSugiTree;
 
 public class BlockSugiSapling extends BlockBush implements IGrowable
 {
-	public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
+    public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
+    protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
 
-	protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
+    public BlockSugiSapling()
+    {
+        this.setUnlocalizedName("sapling.sugi");
+        this.setSoundType(SoundType.PLANT);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(STAGE, Integer.valueOf(0)));
+        this.setCreativeTab(AppalachiaTabs.tabDecoration);
+    }
 
-	public BlockSugiSapling()
-	{
-		this.setUnlocalizedName("sapling.sugi");
-		this.setSoundType(SoundType.PLANT);
-		this.setCreativeTab(AppalachiaTabs.tabBlock);
-		this.setDefaultState(blockState.getBaseState().withProperty(STAGE, Integer.valueOf(0)));
-	}
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return SAPLING_AABB;
+    }
 
-	@Override
-	protected BlockStateContainer createBlockState()
-	{
-		return new BlockStateContainer(this, new IProperty[] {STAGE});
-	}
+    @Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        if (!worldIn.isRemote)
+        {
+            super.updateTick(worldIn, pos, state, rand);
 
-	@Override
-	public IBlockState getStateFromMeta(int meta)
-	{
-		return getDefaultState().withProperty(STAGE, Integer.valueOf((meta & 8) >> 3));
-	}
+            if (worldIn.getLightFromNeighbors(pos.up()) >= 9 && rand.nextInt(7) == 0)
+            {
+                this.grow(worldIn, pos, state, rand);
+            }
+        }
+    }
 
-	@Override
-	public int getMetaFromState(IBlockState state)
-	{
-		int meta = 0;
+    public void grow(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        if (((Integer)state.getValue(STAGE)).intValue() == 0)
+        {
+            worldIn.setBlockState(pos, state.cycleProperty(STAGE), 4);
+        }
+        else
+        {
+            this.generateTree(worldIn, pos, state, rand);
+        }
+    }
 
-		meta |= state.getValue(STAGE).intValue() << 3;
+    public void generateTree(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        if (!TerrainGen.saplingGrowTree(worldIn, rand, pos)) {
+            return;
+        }
 
-		return meta;
-	}
+        WorldGenerator worldgenerator = new WorldGenSugiTree(true);
+        int i = 0;
+        int j = 0;
 
-	@Override
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
-	{
-		return SAPLING_AABB;
-	}
+        IBlockState iblockstate2 = Blocks.AIR.getDefaultState();
 
-	@Override
-	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand)
-	{
-		if (!world.isRemote)
-		{
-			super.updateTick(world, pos, state, rand);
+        worldIn.setBlockState(pos, iblockstate2, 4);
 
-			if (world.getLightFromNeighbors(pos.up()) >= 9 && rand.nextInt(7) == 0)
-			{
-				grow(world, rand, pos, state);
-			}
-		}
-	}
+        if (!worldgenerator.generate(worldIn, rand, pos.add(i, 0, j)))
+        {
+            worldIn.setBlockState(pos, state, 4);
+        }
+    }
 
-	@Override
-	public void grow(World world, Random rand, BlockPos pos, IBlockState state)
-	{
-		if (state.getValue(STAGE).intValue() == 0)
-		{
-			world.setBlockState(pos, state.cycleProperty(STAGE), 4);
-		}
-		else
-		{
-			generateTree(world, pos, state, rand);
-		}
-	}
+    @Override
+    public int damageDropped(IBlockState state)
+    {
+        return 0;
+    }
 
-	public void generateTree(World world, BlockPos pos, IBlockState state, Random rand)
-	{
-		if (!TerrainGen.saplingGrowTree(world, rand, pos))
-		{
-			return;
-		}
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void getSubBlocks(Item itemIn, CreativeTabs tab, List<ItemStack> list)
+    {
+        list.add(new ItemStack(itemIn));
+    }
 
-		world.setBlockState(pos, Blocks.AIR.getDefaultState(), 4);
-		WorldGenSugiTree treegen = new WorldGenSugiTree(false);
-		if (!treegen.generate(world, rand, pos))
-		{
-			world.setBlockState(pos, state, 4);
-		}
-	}
+    @Override
+    public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient)
+    {
+        return true;
+    }
 
-	@Override
-	public boolean canGrow(World world, BlockPos pos, IBlockState state, boolean isClient)
-	{
-		return true;
-	}
+    @Override
+    public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state)
+    {
+        return (double)worldIn.rand.nextFloat() < 0.45D;
+    }
 
-	@Override
-	public boolean canUseBonemeal(World world, Random rand, BlockPos pos, IBlockState state)
-	{
-		return world.rand.nextFloat() < 0.6D;
-	}
+    @Override
+    public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state)
+    {
+        this.grow(worldIn, pos, state, rand);
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return this.getDefaultState().withProperty(STAGE, Integer.valueOf((meta & 8) >> 3));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
+        int i = 0;
+        i = i | ((Integer)state.getValue(STAGE)).intValue() << 3;
+        return i;
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] {STAGE});
+    }
 }
