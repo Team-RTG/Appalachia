11668d7105b02d2a510798278acf8290c9bdb476
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-08-30 18:48:14 +0100

    Experimental tweaks.

--- src/main/java/appalachia/api/AppalachiaBiomes.java
+++ src/main/java/appalachia/api/AppalachiaBiomes.java
@@ -4,7 +4,7 @@ import net.minecraft.world.biome.Biome;
 
 public class AppalachiaBiomes {
 
-	public static Biome appalachianMountains;
+    public static Biome appalachianMountains;
     public static Biome autumnForest;
     public static Biome coveForest;
     public static Biome prairie;
