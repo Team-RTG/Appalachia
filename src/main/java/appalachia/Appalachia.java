e2b5ae29487f791b485ee11ba95088639c4c17b5
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-10-05 23:38:23 +0100

    Moved biome management to AppalachiaBiomeManager.

--- src/main/java/appalachia/Appalachia.java
+++ src/main/java/appalachia/Appalachia.java
@@ -2,13 +2,6 @@ package appalachia;
 
 import java.io.File;
 
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.biome.Biome;
-
-import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.BiomeManager;
-import net.minecraftforge.common.BiomeManager.BiomeEntry;
-import net.minecraftforge.common.BiomeManager.BiomeType;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -19,31 +12,25 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import appalachia.block.BlockManager;
-import appalachia.config.ConfigAppalachia;
 import appalachia.config.ConfigManager;
 import appalachia.event.EventManager;
 import appalachia.item.ItemManager;
 import appalachia.proxy.ClientProxy;
 import appalachia.proxy.CommonProxy;
-import appalachia.util.BiomeUtils;
-import appalachia.util.Logger;
 import appalachia.world.AppalachiaWorldGenerator;
-import appalachia.world.biome.*;
-import appalachia.world.biome.AppalachiaBiome.AppalachiaBiomeProps;
-import static appalachia.api.AppalachiaBiomes.*;
+import appalachia.world.biome.AppalachiaBiomeManager;
 import static appalachia.reference.ModInfo.*;
 
 
 @SuppressWarnings({"WeakerAccess", "unused"})
 @Mod(
-    modid                    = MOD_ID,
-    name                     = MOD_NAME,
-    version                  = MOD_VERSION,
-    dependencies             = "required-after:Forge@[" + MCF_MINVER + "," + MCF_MAXVER + ")" + MOD_DEPS,
+    modid = MOD_ID,
+    name = MOD_NAME,
+    version = MOD_VERSION,
+    dependencies = "required-after:Forge@[" + MCF_MINVER + "," + MCF_MAXVER + ")" + MOD_DEPS,
     acceptableRemoteVersions = "*"
 )
-public class Appalachia
-{
+public class Appalachia {
 
     @Instance(MOD_ID)
     public static Appalachia instance;
@@ -66,13 +53,12 @@ public class Appalachia
         BlockManager.registerBlocks();
         ItemManager.registerItems();
 
-        if (event.getSide().isClient())
-        {
+        if (event.getSide().isClient()) {
             BlockManager.registerModels();
             ItemManager.registerModels();
         }
 
-        addBiomes();
+        AppalachiaBiomeManager.registerBiomes();
     }
 
     @EventHandler
@@ -82,146 +68,12 @@ public class Appalachia
         eventMgr.registerEventHandlers();
 
         BlockManager.addRecipes();
-
         GameRegistry.registerWorldGenerator(new AppalachiaWorldGenerator(), 0);
-
-        doBiomeCheck();
+        AppalachiaBiomeManager.doBiomeCheck();
     }
 
     @EventHandler
     public void initPost(FMLPostInitializationEvent event) {
 
-    }
-
-    private static void registerBiomeWithTypes(Biome biome, String name, int weight, BiomeType btype, BiomeDictionary.Type...types) {
-
-        GameRegistry.register(biome.setRegistryName(new ResourceLocation(MOD_ID, name)));
-        BiomeDictionary.registerBiomeType(biome, types);
-        BiomeManager.addBiome(btype, new BiomeEntry(biome, weight));
-    }
-
-    private void addBiomes() {
-
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // Appalachian Mountains
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        appalachianMountains = new BiomeAppalachianMountains(AppalachiaBiomeProps.APPALACHIAN_MOUNTAINS.getProps());
-        registerBiomeWithTypes(
-            appalachianMountains,
-            "appalachianmountains",
-            ConfigAppalachia.biomeWeight_AppalachianMountains,
-            BiomeType.COOL,
-            BiomeAppalachianMountains.biomeTypes
-        );
-        BiomeManager.addSpawnBiome(appalachianMountains);
-        BiomeManager.addVillageBiome(appalachianMountains, true);
-        BiomeManager.addStrongholdBiome(appalachianMountains);
-        Biome.EXPLORATION_BIOMES_LIST.add(appalachianMountains);
-
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // Autumn Forest
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        autumnForest = new BiomeAutumnForest(AppalachiaBiomeProps.AUTUMN_FOREST.getProps());
-        registerBiomeWithTypes(
-            autumnForest,
-            "autumnforest",
-            ConfigAppalachia.biomeWeight_AutumnForest,
-            BiomeType.COOL,
-            BiomeAutumnForest.biomeTypes
-        );
-        BiomeManager.addSpawnBiome(autumnForest);
-        BiomeManager.addVillageBiome(autumnForest, true);
-        BiomeManager.addStrongholdBiome(autumnForest);
-        Biome.EXPLORATION_BIOMES_LIST.add(autumnForest);
-
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // Cove Forest
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        coveForest = new BiomeCoveForest(AppalachiaBiomeProps.COVE_FOREST.getProps());
-        registerBiomeWithTypes(
-            coveForest,
-            "coveforest",
-            ConfigAppalachia.biomeWeight_CoveForest,
-            BiomeType.COOL,
-            BiomeCoveForest.biomeTypes
-        );
-        BiomeManager.addSpawnBiome(coveForest);
-        BiomeManager.addVillageBiome(coveForest, true);
-        BiomeManager.addStrongholdBiome(coveForest);
-        Biome.EXPLORATION_BIOMES_LIST.add(coveForest);
-
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // Prairie
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        prairie = new BiomePrairie(AppalachiaBiomeProps.PRAIRIE.getProps());
-        registerBiomeWithTypes(
-            prairie,
-            "prairie",
-            ConfigAppalachia.biomeWeight_Prairie,
-            BiomeType.WARM,
-            BiomePrairie.biomeTypes
-        );
-        BiomeManager.addSpawnBiome(prairie);
-        BiomeManager.addVillageBiome(prairie, true);
-        BiomeManager.addStrongholdBiome(prairie);
-        Biome.EXPLORATION_BIOMES_LIST.add(prairie);
-
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // Smoky Mountains
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        smokyMountains = new BiomeSmokyMountains(AppalachiaBiomeProps.SMOKY_MOUNTAINS.getProps());
-        registerBiomeWithTypes(
-            smokyMountains,
-            "smokymountains",
-            ConfigAppalachia.biomeWeight_SmokyMountains,
-            BiomeType.COOL,
-            BiomeSmokyMountains.biomeTypes
-        );
-        BiomeManager.addSpawnBiome(smokyMountains);
-        BiomeManager.addVillageBiome(smokyMountains, true);
-        BiomeManager.addStrongholdBiome(smokyMountains);
-        Biome.EXPLORATION_BIOMES_LIST.add(smokyMountains);
-    }
-
-    private static void doBiomeCheck()
-    {
-        Biome[] b = BiomeUtils.getRegisteredBiomes();
-
-        for (int i = 0; i < 256; i++)
-        {
-            if (b[i] != null)
-            {
-                Biome biome = b[i];
-                int biomeId = BiomeUtils.getId(b[i]);
-                String biomeName = BiomeUtils.getName(b[i]);
-                String biomeClass = b[i].getBiomeClass().getName();
-
-                Logger.debug("Biome (" + biomeId + ") " + biomeName + " from " + biomeClass);
-
-                switch (biomeId) {
-
-                    case 8:     // The Nether
-                    case 9:     // The End
-                    case 127:   // The Void
-
-                        // Do nothing.
-                        break;
-
-                    default:
-
-                        if (biome instanceof AppalachiaBiome) {
-
-                            Logger.info("SUCCESS! %s (%d) was found.", biome.getBiomeName(), BiomeUtils.getId(biome));
-                        }
-
-                        break;
-                }
-            }
-        }
     }
 }
