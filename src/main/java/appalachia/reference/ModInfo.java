c6e74586361f3cf6a3a4599abdfc3cf9a9593081
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-10-09 23:33:19 +0100

    Gradle stuff.

--- src/main/java/appalachia/reference/ModInfo.java
+++ src/main/java/appalachia/reference/ModInfo.java
@@ -1,13 +1,13 @@
 package appalachia.reference;
 
 
-public class ModInfo {
-
-    public static final String MOD_ID = "@MOD_ID@";
-    public static final String MOD_NAME = "@MOD_NAME@";
+public class ModInfo
+{
+    public static final String MOD_ID = "appalachia"; // DO NOT MODIFY! This gets replaced during gradle build.
+    public static final String MOD_NAME = "Appalachia"; // DO NOT MODIFY! This gets replaced during gradle build.
     public static final String MOD_VERSION = "@MOD_VERSION@";
     public static final String MCF_MINVER = "0.0-MCF+MINVER";
     public static final String MCF_MAXVER = "9001.0-MCF+MAXVER";
     public static final String MOD_DEPS = ";after:MODDEPS";
-    public static final String CONFIG_DIRECTORY = MOD_NAME;
+    public static final String CONFIG_DIRECTORY = MOD_ID;
 }
