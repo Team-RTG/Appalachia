99f0cd013399a25143a2c3d62f2982fe1b531def
srs-bsns <forfrdm@gmail.com>
2016-10-24 16:31:04 -0400

    Fix dependencies, readd mcmod.info

--- src/main/java/appalachia/reference/ModInfo.java
+++ src/main/java/appalachia/reference/ModInfo.java
@@ -8,6 +8,7 @@ public class ModInfo
     public static final String MOD_VERSION = "@MOD_VERSION@";
     public static final String MCF_MINVER = "0.0-MCF+MINVER";
     public static final String MCF_MAXVER = "9001.0-MCF+MAXVER";
-    public static final String MOD_DEPS = ";after:MODDEPS";
+    public static final String RTG_MINVER = "0.0-RTG+MINVER";
+    public static final String RTG_MAXVER = "9001.0-RTG+MINVER";
     public static final String CONFIG_DIRECTORY = MOD_NAME;
 }
