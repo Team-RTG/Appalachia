01b6059cdffd69aba12d67a740b109097fad9520
Hamish <moeboy76@gmail.com>
2016-11-17 08:57:26 +1100

    Loot-a-Bear
    - Added functionality for adding loot tables
    - Added Black Bear loot table
    - ran some formatting

--- src/main/java/appalachia/Appalachia.java
+++ src/main/java/appalachia/Appalachia.java
@@ -2,15 +2,6 @@ package appalachia;
 
 import java.io.File;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-
 import appalachia.biome.AppalachiaBiomeManager;
 import appalachia.block.BlockManager;
 import appalachia.client.sound.SoundManager;
@@ -19,24 +10,23 @@ import appalachia.event.EventManager;
 import appalachia.item.ItemManager;
 import appalachia.proxy.ClientProxy;
 import appalachia.proxy.CommonProxy;
+import appalachia.reference.ModInfo;
 import appalachia.rtg.config.BiomeConfigManagerAPL;
 import appalachia.rtg.config.ConfigManagerAPL;
 import appalachia.rtg.world.biome.realistic.appalachia.RealisticBiomeAPLBase;
 import appalachia.world.AppalachiaWorldGenerator;
-import appalachia.reference.ModInfo;
-
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @SuppressWarnings({"WeakerAccess", "unused"})
-@Mod(
-    modid = ModInfo.MOD_ID,
-    name = ModInfo.MOD_NAME,
-    version = ModInfo.MOD_VERSION,
-    dependencies = "required-after:Forge@[" + ModInfo.MCF_MINVER + "," + ModInfo.MCF_MAXVER + ");required-before:RTG@[" + ModInfo.RTG_MINVER + "," + ModInfo.RTG_MAXVER + ")",
-    acceptableRemoteVersions = "*"
-)
+@Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = ModInfo.MOD_VERSION, dependencies = "required-after:Forge@[" + ModInfo.MCF_MINVER + "," + ModInfo.MCF_MAXVER + ");required-before:RTG@[" + ModInfo.RTG_MINVER + "," + ModInfo.RTG_MAXVER + ")", acceptableRemoteVersions = "*")
 public class Appalachia {
 
-    @Instance(ModInfo.MOD_ID)
+    @Mod.Instance(ModInfo.MOD_ID)
     public static Appalachia instance;
     public static String configPath;
     public static EventManager eventMgr;
@@ -46,7 +36,7 @@ public class Appalachia {
 
     private ConfigManager configManager = new ConfigManager();
 
-    @EventHandler
+    @Mod.EventHandler
     public void initPre(FMLPreInitializationEvent event) {
 
         instance = this;
@@ -71,7 +61,7 @@ public class Appalachia {
         ConfigManagerAPL.init(event.getModConfigurationDirectory() + File.separator + rtg.reference.ModInfo.CONFIG_DIRECTORY + File.separator);
     }
 
-    @EventHandler
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
 
         eventMgr = new EventManager();
@@ -84,7 +74,7 @@ public class Appalachia {
         proxy.init(event);
     }
 
-    @EventHandler
+    @Mod.EventHandler
     public void initPost(FMLPostInitializationEvent event) {
 
         RealisticBiomeAPLBase.addBiomes();
