2457eb6d47de8b8c0da720649b754f5cf57f7422
WhichOnesPink <whichonespink@hokecraft.co.uk>
2017-02-26 08:15:06 +0000

    Re-decorated Adirondack Bog.

--- src/main/java/appalachia/rtg/world/biome/deco/collection/DecoCollectionAdirondackBog.java
+++ src/main/java/appalachia/rtg/world/biome/deco/collection/DecoCollectionAdirondackBog.java
@@ -1,9 +1,16 @@
 package appalachia.rtg.world.biome.deco.collection;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+
+import appalachia.api.AppalachiaBlocks;
+import appalachia.api.block.IAppalachiaBlockLeavesFallen;
+import appalachia.block.leaves.fallen.AppalachiaBlockLeavesFallen;
+import appalachia.block.vines.AppalachiaBlockVine;
 import appalachia.rtg.world.biome.deco.DecoAPLTree;
 import appalachia.rtg.world.gen.feature.tree.rtg.*;
 
-import rtg.api.world.deco.DecoTree;
+import rtg.api.world.deco.*;
 import rtg.api.world.deco.collection.DecoCollectionBase;
 
 
@@ -85,7 +92,7 @@ public class DecoCollectionAdirondackBog extends DecoCollectionBase {
         );
 
         // Hackberry.
-        AppalachiaTree treeHackberry = new TreeCeltisOccidentalis();
+        AppalachiaTree treeHackberry = new TreeCeltisOccidentalis(true);
         this.addTree(treeHackberry);
         this.addDeco(new DecoAPLTree(treeHackberry)
                 .setMaxY(treeMaxY)
@@ -93,81 +100,74 @@ public class DecoCollectionAdirondackBog extends DecoCollectionBase {
                 .setTreeCondition(DecoTree.TreeCondition.RANDOM_CHANCE)
                 .setTreeConditionNoise(hackberryNoiseMin)
                 .setTreeConditionNoise2(hackberryNoiseMax)
-                .setTreeConditionChance(6)
+                .setTreeConditionChance(5)
             //.setLoops(1)
         );
 
-//        // Add some fallen trees of the oak and spruce variety (50/50 distribution).
-//        DecoFallenTree decoFallenOak = new DecoFallenTree();
-//        decoFallenOak.setLogCondition(DecoFallenTree.LogCondition.RANDOM_CHANCE);
-//        decoFallenOak.setLogConditionChance(16);
-//        decoFallenOak.setMaxY(80);
-//        decoFallenOak.setLogBlock(AppalachiaBlocks.log_yellow_birch_01.getDefaultState());
-//        decoFallenOak.setLeavesBlock(Blocks.LEAVES.getDefaultState());
-//        decoFallenOak.setMinSize(3);
-//        decoFallenOak.setMaxSize(6);
-//        DecoFallenTree decoFallenSpruce = new DecoFallenTree();
-//        decoFallenSpruce.setLogCondition(DecoFallenTree.LogCondition.RANDOM_CHANCE);
-//        decoFallenSpruce.setLogConditionChance(24);
-//        decoFallenSpruce.setMaxY(80);
-//        decoFallenSpruce.setLogBlock(AppalachiaBlockLog.getRandomLog().getDefaultState());
-//        decoFallenSpruce.setLeavesBlock(AppalachiaBlockLeaves.getRandomLeaves().getDefaultState());
-//        decoFallenSpruce.setMinSize(3);
-//        decoFallenSpruce.setMaxSize(6);
-//        DecoHelper5050 decoFallenTree = new DecoHelper5050(decoFallenOak, decoFallenSpruce);
-//        this.addDeco(decoFallenTree, fallenTrees);
-//
-//        // Shrubs to fill in the blanks.
-//        DecoShrub decoShrubOak = new DecoShrub();
-//        decoShrubOak.setSize(1);
-//        decoShrubOak.setMinY(64);
-//        decoShrubOak.setMaxY(shrubMaxY);
-//        decoShrubOak.setStrengthFactor(8f);
-//        decoShrubOak.setChance(2);
-//        this.addDeco(decoShrubOak);
-//
-//        DecoBoulder decoBoulder1 = new DecoBoulder();
-//        decoBoulder1.setBoulderBlock(Blocks.COBBLESTONE.getDefaultState());
-//        decoBoulder1.setMaxY(80);
-//        decoBoulder1.setChance(16);
-//        decoBoulder1.setStrengthFactor(1f);
-//        this.addDeco(decoBoulder1);
-//
-//        // Fallen leaves.
-//        DecoLayer decoFallenLeaves = new DecoLayer(AppalachiaBlockLeavesFallen.getRandomFallenLeaves().getDefaultState(), IAppalachiaBlockLeavesFallen.LAYERS);
-//        decoFallenLeaves.setLoops(8);
-//        this.addDeco(decoFallenLeaves);
-//
-//        // Very rare 2-block-tall flowers. (TODO: Replace these with Appalachian flora.)
-//        DecoFlowersRTG decoFlowers2 = new DecoFlowersRTG();
-//        decoFlowers2.setFlowers(new int[]{12, 13});
-//        decoFlowers2.setMinY(63);
-//        decoFlowers2.setMaxY(180);
-//        decoFlowers2.setLoops(3);
-//        this.addDeco(decoFlowers2);
-//
-//        // Ferns.
-//        DecoGrass decoFern = new DecoGrass(2);
-//        decoFern.setMinY(63);
-//        decoFern.setMaxY(shrubMaxY);
-//        decoFern.setLoops(2);
-//        this.addDeco(decoFern);
-//
-//        // Poison ivy.
-//        DecoVines decoPoisonIvy = new DecoVines();
-//        decoPoisonIvy.setVineBlock(AppalachiaBlocks.vine_poison_ivy_01);
-//        decoPoisonIvy.setStrengthFactor(12f);
-//        decoPoisonIvy.setPropNorth(AppalachiaBlockVine.NORTH);
-//        decoPoisonIvy.setPropEast(AppalachiaBlockVine.EAST);
-//        decoPoisonIvy.setPropSouth(AppalachiaBlockVine.SOUTH);
-//        decoPoisonIvy.setPropWest(AppalachiaBlockVine.WEST);
-//        this.addDeco(decoPoisonIvy);
-//
-//        // Grass filler.
-//        DecoGrass decoGrass = new DecoGrass();
-//        decoGrass.setMinY(63);
-//        decoGrass.setMaxY(shrubMaxY);
-//        decoGrass.setStrengthFactor(16f);
-//        this.addDeco(decoGrass);
+        // Add some fallen trees using the log blocks of the all the trees in this collection.
+        DecoFallenTree decoFallenTrees = new DecoFallenTree();
+        decoFallenTrees.setLogCondition(DecoFallenTree.LogCondition.RANDOM_CHANCE);
+        decoFallenTrees.setLogConditionChance(16);
+        decoFallenTrees.setMaxY(80);
+        decoFallenTrees.setRandomLogBlocks(this.treeLogs().toArray(new IBlockState[0]));
+        decoFallenTrees.setMinSize(3);
+        decoFallenTrees.setMaxSize(5);
+        this.addDeco(decoFallenTrees, fallenTrees);
+
+        // Shrubs to fill in the blanks.
+        DecoShrub decoShrub = new DecoShrub();
+        decoShrub.setSize(1);
+        decoShrub.setMinY(63);
+        decoShrub.setUseDefaultRandom(true);
+        decoShrub.setRandomLogBlocks(this.treeLogs().toArray(new IBlockState[0]));
+        decoShrub.setRandomLeavesBlocks(this.treeLeaves().toArray(new IBlockState[0]));
+        decoShrub.setMaxY(shrubMaxY);
+        decoShrub.setStrengthFactor(8f);
+        decoShrub.setChance(2);
+        this.addDeco(decoShrub);
+
+        DecoBoulder decoBoulder1 = new DecoBoulder();
+        decoBoulder1.setBoulderBlock(Blocks.COBBLESTONE.getDefaultState());
+        decoBoulder1.setMaxY(80);
+        decoBoulder1.setChance(16);
+        decoBoulder1.setStrengthFactor(1f);
+        this.addDeco(decoBoulder1);
+
+        // Fallen leaves.
+        DecoLayer decoFallenLeaves = new DecoLayer(AppalachiaBlockLeavesFallen.getRandomFallenLeaves().getDefaultState(), IAppalachiaBlockLeavesFallen.LAYERS);
+        decoFallenLeaves.setLoops(8);
+        this.addDeco(decoFallenLeaves);
+
+        // Very rare 2-block-tall flowers. (TODO: Replace these with Appalachian flora.)
+        DecoFlowersRTG decoFlowers2 = new DecoFlowersRTG();
+        decoFlowers2.setFlowers(new int[]{12, 13});
+        decoFlowers2.setMinY(63);
+        decoFlowers2.setMaxY(180);
+        decoFlowers2.setLoops(3);
+        this.addDeco(decoFlowers2);
+
+        // Ferns.
+        DecoGrass decoFern = new DecoGrass(2);
+        decoFern.setMinY(63);
+        decoFern.setMaxY(shrubMaxY);
+        decoFern.setLoops(2);
+        this.addDeco(decoFern);
+
+        // Poison ivy.
+        DecoVines decoPoisonIvy = new DecoVines();
+        decoPoisonIvy.setVineBlock(AppalachiaBlocks.vine_poison_ivy_01);
+        decoPoisonIvy.setStrengthFactor(12f);
+        decoPoisonIvy.setPropNorth(AppalachiaBlockVine.NORTH);
+        decoPoisonIvy.setPropEast(AppalachiaBlockVine.EAST);
+        decoPoisonIvy.setPropSouth(AppalachiaBlockVine.SOUTH);
+        decoPoisonIvy.setPropWest(AppalachiaBlockVine.WEST);
+        this.addDeco(decoPoisonIvy);
+
+        // Grass filler.
+        DecoGrass decoGrass = new DecoGrass();
+        decoGrass.setMinY(63);
+        decoGrass.setMaxY(shrubMaxY);
+        decoGrass.setStrengthFactor(16f);
+        this.addDeco(decoGrass);
     }
 }
