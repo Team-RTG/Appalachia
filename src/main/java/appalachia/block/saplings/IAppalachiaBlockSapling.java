9d8037fd131937a16d5f5a00e625b906a867ba1e
WhichOnesPink <whichonespink@hokecraft.co.uk>
2017-07-16 21:16:31 +0100

    Added IAppalachiaBlockSapling.

---
+++ src/main/java/appalachia/block/saplings/IAppalachiaBlockSapling.java
@@ -0,0 +1,10 @@
+package appalachia.block.saplings;
+
+import appalachia.rtg.world.gen.feature.tree.rtg.AppalachiaTree;
+
+/**
+ * Created by WhichOnesPink on 16/07/2017.
+ */
+public interface IAppalachiaBlockSapling {
+    AppalachiaTree getTree();
+}
