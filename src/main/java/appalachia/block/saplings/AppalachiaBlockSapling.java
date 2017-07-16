05c46ce41afa9193d408dc6580afaf8e73fea405
WhichOnesPink <whichonespink@hokecraft.co.uk>
2017-07-16 22:16:11 +0100

    AppalachiaBlockSapling is now abstract & implements IAppalachiaBlockSapling.

--- src/main/java/appalachia/block/saplings/AppalachiaBlockSapling.java
+++ src/main/java/appalachia/block/saplings/AppalachiaBlockSapling.java
@@ -29,7 +29,7 @@ import appalachia.block.IAppalachiaBlock;
 import appalachia.gui.AppalachiaTabs;
 import appalachia.rtg.world.gen.feature.tree.rtg.AppalachiaTree;
 
-public class AppalachiaBlockSapling extends BlockBush implements IGrowable, IAppalachiaBlock {
+public abstract class AppalachiaBlockSapling extends BlockBush implements IGrowable, IAppalachiaBlock, IAppalachiaBlockSapling {
 
     public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
     protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
