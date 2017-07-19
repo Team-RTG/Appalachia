36e33cecb1ca66cf1297da96319d1eb807f12263
WhichOnesPink <whichonespink@hokecraft.co.uk>
2017-07-19 07:27:42 +0100

    Re-enabled common decorations.

--- src/main/java/appalachia/rtg/world/biome/deco/collection/DecoCollectionAPL.java
+++ src/main/java/appalachia/rtg/world/biome/deco/collection/DecoCollectionAPL.java
@@ -1,6 +1,9 @@
 package appalachia.rtg.world.biome.deco.collection;
 
+import appalachia.rtg.world.biome.deco.DecoPlants;
+
 import rtg.api.config.BiomeConfig;
+import rtg.api.world.deco.DecoGrass;
 import rtg.api.world.deco.collection.DecoCollectionBase;
 
 
@@ -13,25 +16,25 @@ public class DecoCollectionAPL extends DecoCollectionBase {
 
         super(config);
 
-//        // Plants
-//        DecoPlants decoPlants = new DecoPlants();
-//        decoPlants.setMinY(63);
-//        decoPlants.setMaxY(foliageMaxY);
-//        decoPlants.setLoops(4);
-//        this.addDeco(decoPlants);
-//
-//        // Ferns.
-//        DecoGrass decoFern = new DecoGrass(2);
-//        decoFern.setMinY(63);
-//        decoFern.setMaxY(foliageMaxY);
-//        decoFern.setLoops(2);
-//        this.addDeco(decoFern);
-//
-//        // Grass filler.
-//        DecoGrass decoGrass = new DecoGrass();
-//        decoGrass.setMinY(63);
-//        decoGrass.setMaxY(foliageMaxY);
-//        decoGrass.setStrengthFactor(16f);
-//        this.addDeco(decoGrass);
+        // Plants
+        DecoPlants decoPlants = new DecoPlants();
+        decoPlants.setMinY(63);
+        decoPlants.setMaxY(foliageMaxY);
+        decoPlants.setLoops(4);
+        this.addDeco(decoPlants);
+
+        // Ferns.
+        DecoGrass decoFern = new DecoGrass(2);
+        decoFern.setMinY(63);
+        decoFern.setMaxY(foliageMaxY);
+        decoFern.setLoops(2);
+        this.addDeco(decoFern);
+
+        // Grass filler.
+        DecoGrass decoGrass = new DecoGrass();
+        decoGrass.setMinY(63);
+        decoGrass.setMaxY(foliageMaxY);
+        decoGrass.setStrengthFactor(16f);
+        this.addDeco(decoGrass);
     }
 }
